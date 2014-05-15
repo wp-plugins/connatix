440dd80714ff42751de9fdf15bc027e59c12fddd
alexandru.manea <alexandru.manea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-15 04:30:05 +0000

    tagging version 2.5.0
    git-svn-id: https://plugins.svn.wordpress.org/connatix/trunk@914353 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ widgets/connatix.widget.infeed.php
@@ -0,0 +1,31 @@
+<?php
+class Connatix_Widget_Infeed extends WP_Widget {
+
+	public function __construct() {
+        parent::__construct(
+			'connatix_infeed_widget', // Base ID
+			'Connatix InFeed Widget', // Name
+			array( 'description' => 'Infeed Connatix Integration')
+		);
+	}
+
+	public function widget( $args, $instance ) {
+        $title = apply_filters( 'widget_title', $instance['title'] );
+
+        echo $args['before_widget'];
+        
+        $options = get_option(ConnatixJSPlugin::$OPTIONS_KEY);
+        if($options != null)
+            echo "<script type='text/javascript' src='http://cdn.connatix.com/min/connatix.renderer.infeed.min.js' mode='fast' data-connatix-token='".$options->_token."'></script>";
+        
+        echo $args['after_widget'];
+	}
+
+	public function form( $instance ) {
+        
+	}
+
+	public function update( $new_instance, $old_instance ) {
+        
+	}
+}
