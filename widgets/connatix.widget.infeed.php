add10095cad3b1506a1d9a011229f4ebd86a9c64
alexandru.manea <alexandru.manea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-15 22:01:01 +0000

    tagging version 2.5.1
    git-svn-id: https://plugins.svn.wordpress.org/connatix/trunk@915007 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widgets/connatix.widget.infeed.php
+++ widgets/connatix.widget.infeed.php
@@ -10,12 +10,17 @@ class Connatix_Widget_Infeed extends WP_Widget {
 	}
 
 	public function widget( $args, $instance ) {
+        global $wp_query; 
+        $post_id = $wp_query->post->ID;
+        
         $title = apply_filters( 'widget_title', $instance['title'] );
+        
 
         echo $args['before_widget'];
         
         $options = get_option(ConnatixJSPlugin::$OPTIONS_KEY);
-        if($options != null)
+        
+        if($options != null && isset($options->_id) && $options->_id != $post_id)
             echo "<script type='text/javascript' src='http://cdn.connatix.com/min/connatix.renderer.infeed.min.js' mode='fast' data-connatix-token='".$options->_token."'></script>";
         
         echo $args['after_widget'];
