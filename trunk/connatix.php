5469abe21a17303c16bf9b4a88f6603ba2a2dd12
alexandru.manea <alexandru.manea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-12 13:20:58 +0000

    v3.1.7
    git-svn-id: https://plugins.svn.wordpress.org/connatix@1065919 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/connatix.php
+++ trunk/connatix.php
@@ -3,7 +3,7 @@
  * Plugin Name: Connatix
  * Plugin URI: http://connatix.com/
  * Description: This plugin will offer you an easy way to set up your CONNATIX ads. Use Settings to set up your options.
- * Version: 3.1.6
+ * Version: 3.1.7
  * Author: Connatix
  * Author URI: http://connatix.com
  * License: GPL2
@@ -25,7 +25,7 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
  */
-define("CONNATIX_VERSION", "v3.1.6");
+define("CONNATIX_VERSION", "v3.1.7");
 
 
 
