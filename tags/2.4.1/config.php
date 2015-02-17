dce8e709be27544516a19365925bbc6bd2c28d32
alexandru.manea <alexandru.manea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-17 12:12:47 +0000

    fixed_tag
    git-svn-id: https://plugins.svn.wordpress.org/connatix/tags/3.2.0@1092371 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/2.4.1/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('CONNATIX_PLUGIN_URL', plugin_dir_url( __FILE__ ));
-
-define("CONNATIX_PARTNER_API", "http://api.connatix.com/partner/get.json");
-
--- tags/2.5.0/js/connatix.replacer.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-    jQuery("#save-button").click(function(){
-        jQuery(".connatix-preloader").show();
-    });
-    
-    jQuery(".delete-button").click(function(){
-        jQuery(this).parent().parent().remove();
-    });
-});
