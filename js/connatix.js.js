6dfe69c556e3d4c7fd25161b8268115bfa8a883c
alexandru.manea <alexandru.manea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-17 08:18:42 +0000

    v3.3.0
    git-svn-id: https://plugins.svn.wordpress.org/connatix/trunk@1182410 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/connatix.js.js
+++ js/connatix.js.js
@@ -1,31 +1,38 @@
 var connatix = {
-    bind: function(){
+    bind: function () {
         //bind of the go to page functionality
-        connatix.jQuery("select[name=categoryID]").change(function(){
+        connatix.jQuery("select[name=categoryID]").change(function () {
             var url = connatix.jQuery(this).find("option:selected").attr("data-link");
             connatix.jQuery(".category-page-link").attr("href", url);
         });
-        
+
         this.refreshAdUnit();
-        connatix.jQuery(".ad-unit").find("input[name=skip_adunit]").click(function(){
+        connatix.jQuery(".ad-unit").find("input[name=skip_adunit]").off('click').on('click', function () {
             connatix.refreshAdUnit();
         });
-        
-        connatix.jQuery(".ad-unit").find("select[data-ignore=true]").change(function(){
+
+        connatix.jQuery(".ad-unit").find("select[data-ignore=true]").change(function () {
             connatix.refreshAdUnit();
         });
-        
-        connatix.jQuery(".connatix-ad-delete").click(function(){
-           if(confirm("Do you really want to delete this ad unit ?"))
-               return true;
-           return false;
-            
+
+        connatix.jQuery(".connatix-ad-delete").off('click').on('click', function () {
+            if (confirm("Do you really want to delete this ad unit ?"))
+                return true;
+            return false;
+
+        });
+
+        connatix.jQuery('#connatix-textarea-code').hide();
+
+        connatix.jQuery('#connatix-custom-code').off('click').on('click', function () {
+            connatix.jQuery('#connatix-custom-code').toggle();
+            connatix.jQuery('#connatix-textarea-code').toggle();
         });
     },
-    refreshAdUnit: function()
+    refreshAdUnit: function ()
     {
         var form = connatix.jQuery(connatix.jQuery(".ad-unit")[0]);
-        if(form.find("input[data-ignore=true]").length > 0)
+        if (form.find("input[data-ignore=true]").length > 0)
         {
             var checked = form.find("input[data-ignore=true]").is(":checked");
 
@@ -33,12 +40,12 @@ var connatix = {
             form.find("select").prop('disabled', checked);
 
             form.find("input[data-ignore=true]").prop("disabled", false);
-        }else 
+        } else
         //if the page is using a select for the ad unit (inpost)    
-        if(form.find("select[data-ignore=true]"))
+        if (form.find("select[data-ignore=true]"))
         {
             var checked = parseInt(form.find("select[data-ignore=true]").val()) > 0;
-            
+
             form.find("input").prop('disabled', checked);
             form.find("select").prop('disabled', checked);
 
@@ -50,6 +57,6 @@ var connatix = {
 
 //Bootstrap
 connatix.jQuery = jQuery.noConflict();
-connatix.jQuery(function(){
+connatix.jQuery(function () {
     connatix.bind();
 });
